a8bf63bd2892837b607d04b356b3f618088296ae
Naman Nirwal <87256361+namannirwal@users.noreply.github.com>
2023-08-06 09:40:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -53,20 +53,20 @@ if(isset($_POST['name'])){
 <body>
     <img class="bg" src="bg.jpg" alt="Trip">
     <div class="container">
-        <h1>Welcome to US Trip form</h3>
-        <p>Enter your details and submit this form to confirm your participation in the trip </p>
+        <h1>Welcome to Mussoorie Trip form</h3>
+        <p>Enter your details and submit this form to confirm your presence in the trip </p>
         <?php
         if($insert == true){
-        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
+        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us.</p>";
         }
     ?>
         <form action="index.php" method="post">
-            <input type="text" name="name" id="name" placeholder="Enter your name">
-            <input type="text" name="age" id="age" placeholder="Enter your Age">
-            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
-            <input type="email" name="email" id="email" placeholder="Enter your email">
-            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
-            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <input type="text" name="name" id="name" placeholder="Please,Enter your name">
+            <input type="text" name="age" id="age" placeholder="Please,Enter your Age">
+            <input type="text" name="gender" id="gender" placeholder="Please,Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Please,Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Please,Enter your phone">
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Please,Enter any other information here"></textarea>
             <button class="btn">Submit</button> 
         </form>
     </div>
